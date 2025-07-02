692ea63f3c4b2cea4f41d21ff9e7cda0fbb7c1ca
Otabek <otashqashqar@gmail.com>
2025-07-02 08:47:35 +0500

    add requirements.txt file

--- ucode/settings.py
+++ ucode/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(nbn9nm&!yh#x9x+vxykn@slky1i(@0rqqyk7&7f7ml49yp*ln'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
